5245590ba345cbfcdbe5489e935fe35b8ed798ae
Daniel Hsing <hsing.daniel@gmail.com>
2018-11-01 16:25:54 -0400

    feat: update website metadata
    Signed-off-by: Daniel Hsing <hsing.daniel@gmail.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Daniel Hsing',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Daniel Hsing',
+        short_name: 'DH',
         start_url: '/',
         background_color: '#663399',
         theme_color: '#663399',
